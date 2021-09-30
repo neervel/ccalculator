5b28f55680e5940ec77bbc9a4cacfeb9858541c1
Neervel <klihopoy@mail.ru>
2021-09-30 10:15:49 +0300

    add calculator component

---
+++ src/components/Calculator.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="calc-block">
+    <div class="calc-inputs">
+      <div class="calc-inputs-header">
+        <span>Ингредиент</span>
+        <span>Ккал</span>
+        <span>Белки</span>
+        <span>Жиры</span>
+        <span>Углеводы</span>
+      </div>
+      <div class="calc-inputs-item">
+        <input type="text">
+        <input type="number" value="0" min="0" step="0.1">
+        <input type="number" value="0" min="0" step="0.1">
+        <input type="number" value="0" min="0" step="0.1">
+        <input type="number" value="0" min="0" step="0.1">
+      </div>
+    </div>
+    <div class="calc-result"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Calculator'
+}
+</script>
